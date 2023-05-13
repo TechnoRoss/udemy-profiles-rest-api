c3125216a79824b6d1c9f36870828ea9631f680d
TechnoRoss <TechnoRoss@users.noreply.github.com>
2023-05-13 13:47:51 -0500

    authentication and permissions

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -10,7 +10,7 @@ class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.UserProfile
         fields = ('id', 'email', 'name', 'password')
-        extra_keywords_args = {
+        extra_kwargs = {
             'password': {
                 'write_only': True,
                 'style': { 'input_type': 'password' }
