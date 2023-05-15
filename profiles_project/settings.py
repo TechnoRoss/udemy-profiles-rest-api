13f75c0312e299d565145b4c9f50564ed541970e
TechnoRoss <TechnoRoss@users.noreply.github.com>
2023-05-15 14:37:59 -0500

    deploy

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+#tjy4(x9g9mfp(&3h7g=1(ur^ax=kq8*_2rpr#w(^0!sf&-31'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environment.get('DEBUG'), 1))
 
 ALLOWED_HOSTS = []
 
@@ -123,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+STATIC_ROOT = 'static/'
